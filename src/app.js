3d21f24b5a702c35b0cc6065925ac964f457bd44
navyumm <navyum12@gmail.com>
2025-06-25 14:36:50 +0530

    created todo controller

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 const app = express();
+import todoRoutes from "./routes/todoRoutes.js"
 
 
 app.use(express.json());
+
+app.use('/api/v1/todos', todoRoutes)
 
 
 export default app;
